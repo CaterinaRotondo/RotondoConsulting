3b3870c1326565073c250f9a9f6ed45ef130f064
Caterina Rotondo <caterina.rotondo@dap-systems.de>
2025-08-06 11:04:06 +0200

    Fixed carousel settings

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -15,7 +15,7 @@ export class CarouselComponent implements AfterViewInit {
       image: 'assets/ha-consult.png',
       title: 'ha consult',
       link: 'https://ha-consult.de/'
-    },
+    } /*,
     {
       image: 'assets/computer.png',
       title: 'Projekt 2',
@@ -30,21 +30,21 @@ export class CarouselComponent implements AfterViewInit {
       image: 'assets/computer.png',
       title: 'Projekt 4',
       link: 'https://example2.com'
-    }
+    }*/
   ];
 
   ngAfterViewInit(): void {
     const totalSlides = this.projects.length;
     const perPageDefault = 3;
 
-     const loopEnabled = totalSlides > perPageDefault;
+    const loopEnabled = totalSlides > perPageDefault;
 
     new Splide('#carousel', {
       type: loopEnabled ? 'loop' : 'slide',
-      perPage: perPageDefault,
+      perPage: totalSlides > perPageDefault ? perPageDefault : totalSlides,
       perMove: 1,
       breakpoints: {
-        1600: { perPage: 2 },
+        1600: { perPage: totalSlides > 2 ? 2 : totalSlides },
         920: { perPage: 1}
         /* 768: { perPage: 1 } */
       },
